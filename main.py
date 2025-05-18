0c93d887d05f515c88ce577d0c4fbd802d96b2c6
CintyaNakariJi <cintyanakari.1@gmail.com>
2025-05-18 00:57:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-# main.py
 from fastapi import FastAPI
 from influxdb_client import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS
 import os
 
 app = FastAPI()
 
-# Configura tu conexión
-url = os.getenv("https://us-east-1-1.aws.cloud2.influxdata.com")
-token = os.getenv("7QJseS0ubDwewDVp7F4j2CwJLI8Gncesop0J6lyQnH-EiUv8BgeHSHtjqb_4td5l4LKAPqLLVxygFOWYe-svWQ==")
-org = os.getenv("Trabajos")
-bucket = os.getenv("Monitoreo_de_cultivos_maiz")
+# Configura tu conexión con variables de entorno
+url = os.getenv("INFLUX_URL")
+token = os.getenv("INFLUX_TOKEN")
+org = os.getenv("INFLUX_ORG")
+bucket = os.getenv("INFLUX_BUCKET")
 
+# Crear cliente de InfluxDB
 client = InfluxDBClient(url=url, token=token, org=org)
 
 @app.get("/api/datos")
 def leer_datos():
     query = f'from(bucket:"{bucket}") |> range(start: -7d)'
     result = client.query_api().query(org=org, query=query)
-    
+
     datos = []
     for table in result:
         for record in table.records:
             datos.append({
                 "time": record.get_time().isoformat(),
                 "valor": record.get_value(),
-                "medicion": record.get_measurement()  # <--- esto es lo nuevo
+                "medicion": record.get_measurement()
             })
     return {"datos": datos}
